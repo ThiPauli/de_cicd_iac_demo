1637b023cee954bff9443dadf1467afd91f95e1b
thiago.pauli <thiago.pauli26@gmail.com>
2024-05-24 10:24:07 -0300

    changed: files .py fixed by pre-commit

--- dags/test_dag_ci_fail.py
+++ dags/test_dag_ci_fail.py
@@ -1,50 +1,48 @@
-from airflow import DAG
-
-
-
-
 from datetime import datetime, timedelta
 
-
+from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
 # Define the default_args dictionary. These args will be passed to each operator.
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
+
 
 # Define a simple function that will be run by the PythonOperator
 def print_hello():
     print("Hello World")
 
+
 def print_goodbye():
     print("Goodbye World")
 
+
 # Create the DAG
 dag = DAG(
-    'test_dag_ci_fail',
+    "test_dag_ci_fail",
     default_args=default_args,
-    description='A simple DAG',
+    description="A simple DAG",
     schedule_interval=None,
     start_date=datetime(2023, 1, 1),
     catchup=False,
-    tags=["etl", "demo-cicd", "test"]
+    tags=["etl", "demo-cicd", "test"],
 )
 
 # Define the tasks
 task1 = PythonOperator(
-    task_id='print_hello',
+    task_id="print_hello",
     python_callable=print_hello,
     dag=dag,
 )
 
 task2 = PythonOperator(
-    task_id='print_goodbye',
+    task_id="print_goodbye",
     python_callable=print_goodbye,
     dag=dag,
 )
--- tests/dags_validation_tests/test_dag_integrity.py
+++ tests/dags_validation_tests/test_dag_integrity.py
@@ -1,20 +1,25 @@
 import os
+
 import pytest
 from airflow.models import DagBag
 
 # Define the path to the dags folder
-DAGS_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../dags'))
+DAGS_FOLDER = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../../dags")
+)
 
 # Initialize the DagBag object
 dag_bag = DagBag(dag_folder=DAGS_FOLDER, include_examples=False)
+
 
 def get_dag_file_names():
     dag_files = []
     for root, _, files in os.walk(DAGS_FOLDER):
         for file in files:
-            if file.endswith('.py'):
+            if file.endswith(".py"):
                 dag_files.append(os.path.join(root, file))
     return dag_files
+
 
 @pytest.mark.parametrize("dag_file", get_dag_file_names())
 def test_dag_id_matches_file_name(dag_file):
@@ -22,11 +27,16 @@ def test_dag_id_matches_file_name(dag_file):
     test if a DAG ID is equal the filename
     """
 
-    dag_id = os.path.basename(dag_file).replace('.py', '')
-    assert dag_id in dag_bag.dags, f"DAG ID '{dag_id}' not found in DagBag for file '{dag_file}'"
+    dag_id = os.path.basename(dag_file).replace(".py", "")
+    assert (
+        dag_id in dag_bag.dags
+    ), f"DAG ID '{dag_id}' not found in DagBag for file '{dag_file}'"
 
     dag = dag_bag.get_dag(dag_id)
-    assert dag.dag_id == dag_id, f"DAG ID '{dag.dag_id}' does not match file name '{dag_file}'"
+    assert (
+        dag.dag_id == dag_id
+    ), f"DAG ID '{dag.dag_id}' does not match file name '{dag_file}'"
+
 
 @pytest.mark.parametrize("dag_id", dag_bag.dag_ids)
 def test_dag_has_specific_tags(dag_id):
@@ -40,5 +50,8 @@ def test_dag_has_specific_tags(dag_id):
     missing_tags = required_tags - dag_tags
     assert not missing_tags, f"DAG '{dag_id}' is missing tags: {missing_tags}"
 
+
 def test_no_import_errors():
-    assert len(dag_bag.import_errors) == 0, f"Total import failures: {len(dag_bag.import_errors)}"
+    assert (
+        len(dag_bag.import_errors) == 0
+    ), f"Total import failures: {len(dag_bag.import_errors)}"
